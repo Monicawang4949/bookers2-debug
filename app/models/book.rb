086cd988ddcc1e8b49d4dd05bf63a9923fcb393d
Monica <167148997+Monicawang4949@users.noreply.github.com>
2024-06-05 05:54:56 +0000

    7a8a9a

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,7 +1,9 @@
 class Book < ApplicationRecord
   belongs_to :user
   has_many :favorites, dependent: :destroy
+  has_many :week_favorites, -> { where(created_at: 1.week.ago.beginning_of_day..Time.current.end_of_day) }
   has_many :book_comments, dependent: :destroy
+  has_many :read_counts, dependent: :destroy
 
   validates :title,presence:true
   validates :body,presence:true,length:{maximum:200}
