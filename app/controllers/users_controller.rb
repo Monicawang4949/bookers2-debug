2b72688f0d279073206ed5cf7ecb78d4e008203a
Monica <167148997+Monicawang4949@users.noreply.github.com>
2024-06-06 07:18:02 +0000

    7b8b9b

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,10 @@ class UsersController < ApplicationController
     @books = @user.books.all
     @book = Book.new
     @book_comment = BookComment.new
+    @today_book = @user.books.created_today
+    @yesterday_book = @user.books.created_yesterday
+    @this_week_book = @user.books.created_this_week
+    @last_week_book = @user.books.created_last_week
   end
 
   def index
@@ -25,6 +29,12 @@ class UsersController < ApplicationController
     else
       render :edit
     end
+  end
+
+  def posts_on_date
+    user = User.includes(:books).find(params[:user_id])
+    @books = user.books.where(created_at: params[:created_at].to_date.all_day)
+    render :posts_on_date_form
   end
 
   private
